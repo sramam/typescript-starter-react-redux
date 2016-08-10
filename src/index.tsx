122ea9880657422d6a5f81ae68d104984e69765f
Deepa Ramam <dshenoy@gmail.com>
2016-08-10 14:43:11 -0700

    Added redux. At this point, a store is being created and passed down to the components using <Provider>. Next : connect to the store.

--- src/index.tsx
+++ src/index.tsx
@@ -8,14 +8,38 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Router, Route, Link, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import configureStore from './store';
 
 import App from './containers/App/App';
 import Hello from './components/Hello/Hello';
 
+const selectLocationState = () => {
+  let prevRoutingState;
+
+  return (state) => {
+    const routingState = state.route; // or state.route
+
+    if (routingState !== prevRoutingState) {
+      prevRoutingState = routingState;
+    }
+
+    return prevRoutingState;
+  };
+};
+
+const store = configureStore({}, browserHistory);
+const history = syncHistoryWithStore(browserHistory, store, {
+  selectLocationState: selectLocationState(),
+});
+
 ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path='/' component={App}>
-      <Route path='Hello' component={Hello}/>
-    </Route>
-  </Router>
+  <Provider store={ store }>
+    <Router history={ history }>
+      <Route path='/' component={App}>
+        <Route path='Hello' component={Hello}/>
+      </Route>
+    </Router>
+  </Provider>
 ), document.getElementById('app'));
---
+++ src/store.ts
@@ -0,0 +1,47 @@
+/**
+ * Create the store with asynchronously loaded reducers
+ */
+
+import { createStore, applyMiddleware, compose } from 'redux';
+import { routerMiddleware } from 'react-router-redux';
+import createReducer from './reducers';
+
+const devtools = window.devToolsExtension || (() => noop => noop);
+
+export default function configureStore(initialState = {}, history) {
+  // Create the store with two middlewares
+  // 1. sagaMiddleware: Makes redux-sagas work
+  // 2. routerMiddleware: Syncs the location/URL path to the state
+  const middlewares = [
+    routerMiddleware(history),
+  ];
+
+  const enhancers = [
+    applyMiddleware(...middlewares),
+    devtools(),
+  ];
+
+  const store = createStore(
+    createReducer(),
+    compose(applyMiddleware(...middlewares),
+    devtools())
+  );
+
+  // Extensions
+  store.asyncReducers = {}; // Async reducer registry
+
+  // Make reducers hot reloadable, see http://mxs.is/googmo
+  /* istanbul ignore next */
+  if (module.hot) {
+    module.hot.accept('./reducers', () => {
+      System.import('./reducers').then((reducerModule) => {
+        const createReducers = reducerModule.default;
+        const nextReducers = createReducers(store.asyncReducers);
+
+        store.replaceReducer(nextReducers);
+      });
+    });
+  }
+
+  return store;
+}
